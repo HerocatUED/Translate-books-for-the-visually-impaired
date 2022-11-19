a3a407d20d7d942a29fe28cb03e5b46d8511b4b7
HerocatUED <1722791503@qq.com>
2022-11-19 22:14:56 +0800

    process page by page in order to typeset the words

--- OCR/paddleOCR.py
+++ OCR/paddleOCR.py
@@ -1,12 +1,29 @@
 from paddleocr import PaddleOCR
+from PIL import Image
+import cv2
+import fitz
+import numpy as np
 
-# Paddleocr目前支持的多语言语种可以通过修改lang参数进行切换
-# 例如`ch`, `en`, `fr`, `german`, `korean`, `japan`
-# need to run only once to download and load model into memory
 ocr = PaddleOCR(use_angle_cls=True, use_gpu=True,
-                lang="ch", page_num=6, show_log=False)
+                lang="ch", page_num=2, show_log=False)
+
 img_path = './Datasets/敦煌奇幻旅行记1/1-68.pdf'
-result = ocr.ocr(img_path, cls=True, det=True)
+
+imgs = []
+with fitz.open(img_path) as pdf:
+    for pg in range(0, pdf.pageCount):
+        page = pdf[pg]
+        mat = fitz.Matrix(2, 2)
+        pm = page.getPixmap(matrix=mat, alpha=False)
+        # if width or height > 2000 pixels, don't enlarge the image
+        if pm.width > 2000 or pm.height > 2000:
+            pm = page.getPixmap(matrix=fitz.Matrix(1, 1), alpha=False)
+        img = Image.frombytes("RGB", [pm.width, pm.height], pm.samples)
+        img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+        imgs.append(img)
+
+result = ocr.ocr(imgs[10], cls=True, det=True)
+
 txt = ""
 for idx in range(len(result)):
     res = result[idx]
@@ -14,5 +31,3 @@ for idx in range(len(result)):
         txt = txt+line[1][0]+"\n"
 f = open('./OCR/test.txt', 'w')
 f.write(txt)
-
-
