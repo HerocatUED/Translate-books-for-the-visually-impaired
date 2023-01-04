36bb01ade04c0c8d052419a58528ab04f5603119
HerocatUED <1722791503@qq.com>
2023-01-04 09:49:50 +0800

    reorgnize

---
+++ ImageSegmentation.py
@@ -0,0 +1,8 @@
+from scipy import ndimage, spatial
+import numpy as np
+import cv2
+
+def ImageSegment(img):
+    coordinates=[]
+    imgs=[]
+    return coordinates,imgs
---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+from DataPreproduce import preproduce
+from MyOCR import OCR
+from ImageSegmentation import ImageSegment
+
+book_paths = os.listdir('input')
+for book_path in book_paths:
+    bookname = book_path[:-4]
+    pages=preproduce(book_path)
+    img_coordinates,imgs=ImageSegment(pages)
+    OCR(pages,bookname,img_coordinates)
